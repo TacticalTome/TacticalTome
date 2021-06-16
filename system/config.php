f22c1b12a5f81c64959dbf7b088b5d1c1a581aee
Paughton <tanktotgames@gmail.com>
2021-06-15 19:19:50 -0500

    Added main email to the config and formatted the config file

--- system/config.php
+++ system/config.php
@@ -1,14 +1,32 @@
 <?php
 	
+	/*
+		Sessions
+	*/
 	session_start();
+
+	/*
+		PHP Settings
+	*/
 	error_reporting(E_ALL);
 	ini_set("allow_url_fopen", true);
 
+	/*
+		URL
+	*/
 	define("URL", "http://" . $_SERVER['HTTP_HOST'] . "/gamershandbook/");
 	define("HTTPS_ENABLED", false);
+
+	/*
+		Website Properties
+	*/
 	define("WEBSITE_NAME", "Gamer's Handbook");
+	define("WEBSITE_EMAIL", "tanktotgames@gmail.com");
 	define("ALLOW_REGISTRATION", true);
 
+	/*
+		Directories
+	*/
 	define("ROOT_DIRECTORY", $_SERVER['DOCUMENT_ROOT'] . "/gamershandbook/");
 	define("SYSTEM_DIRECTORY", ROOT_DIRECTORY . "system/");
 	define("IMAGE_DIRECTORY", ROOT_DIRECTORY . "images/");
@@ -20,6 +38,9 @@
 	define("BANNER_DIRECTORY", IMAGE_DIRECTORY . "banners/");
 	define("COVER_DIRECTORY", IMAGE_DIRECTORY . "covers/");
 
+	/*
+		Misc.
+	*/
 	date_default_timezone_set("America/Chicago");
 	
 ?>
