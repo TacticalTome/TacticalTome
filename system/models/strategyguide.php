da3f8b443912a9a08c4aef144bf51e4f1b0599ad
Paughton <tanktotgames@gmail.com>
2021-07-07 00:47:33 -0500

    Removed unnecessary parameter santiation from the static delete method of the StrateyGuide model

--- system/models/strategyguide.php
+++ system/models/strategyguide.php
@@ -127,8 +127,6 @@
         }
 
         static public function delete(Database $database, int $id): void {
-            $id = $database->protect($id);
-
             $delete = $database->query("DELETE FROM strategyguides WHERE id='$id'");
         }
     }
