96c785b85a4127134e962105943e362eaf0d519e
Paughton <tanktotgames@gmail.com>
2021-07-07 20:11:29 -0500

    Uploaded tacticaltome.sql

---
+++ tacticaltome.sql
@@ -0,0 +1,153 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 08, 2021 at 03:00 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gamershandbook`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `confirmations`
+--
+
+CREATE TABLE `confirmations` (
+  `id` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `action` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `value` varchar(255) NOT NULL,
+  `time` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `games`
+--
+
+CREATE TABLE `games` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `description` text NOT NULL,
+  `developer` varchar(100) NOT NULL,
+  `tags` varchar(100) NOT NULL,
+  `banner` varchar(100) NOT NULL,
+  `cover` varchar(1024) NOT NULL,
+  `steamappid` int(11) NOT NULL,
+  `news` tinyint(1) NOT NULL,
+  `followers` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `strategyguides`
+--
+
+CREATE TABLE `strategyguides` (
+  `id` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `gid` int(11) NOT NULL,
+  `title` varchar(256) NOT NULL,
+  `content` text NOT NULL,
+  `timecreated` int(11) NOT NULL,
+  `favorites` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `timecreated` int(11) NOT NULL,
+  `activated` tinyint(1) NOT NULL,
+  `lastposted` int(11) NOT NULL,
+  `followedgames` varchar(100) NOT NULL,
+  `favoritestrategyguides` varchar(100) NOT NULL,
+  `moderator` tinyint(1) NOT NULL,
+  `banned` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `confirmations`
+--
+ALTER TABLE `confirmations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `games`
+--
+ALTER TABLE `games`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `strategyguides`
+--
+ALTER TABLE `strategyguides`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `confirmations`
+--
+ALTER TABLE `confirmations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `games`
+--
+ALTER TABLE `games`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `strategyguides`
+--
+ALTER TABLE `strategyguides`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
