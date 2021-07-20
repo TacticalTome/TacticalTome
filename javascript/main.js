b8a5d345147875a53f324d599b01cce866a9ea03
Paughton <tanktotgames@gmail.com>
2021-07-20 18:59:54 -0500

    Uploaded new javascript files

---
+++ javascript/main.js
@@ -0,0 +1,11 @@
+/*
+    Imports
+*/
+import {TopNavigation} from "./framework/topnavigation.js";
+
+/*
+    When window is loaded
+*/
+window.onload = function() {
+    const topNavbar = new TopNavigation("navigationCollapse");
+}
